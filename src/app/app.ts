ed1136e20c8cb8803e5b0d153f610a082b67c0aa
Noelia Navas <noelianavasgarrido96@gmail.com>
2026-02-16 21:53:55 +0100

    Añadimos GitHub Actions deploy

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { PostsService } from './services/posts.js'; 
+import { Post } from './models/post.js';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('posts-get');
+export class AppComponent {
+  posts$: Observable<Post[]>;
+
+  constructor(private postsService: PostsService) {
+    this.posts$ = this.postsService.getAll();
+  }
 }
